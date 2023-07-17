cdfded167eac3b29b8403342f7c6072b63484821
Hoang Nhan <hnhan793@gmail.com>
2023-07-17 15:57:09 +0700

    note comment

--- controllers/functions.js
+++ controllers/functions.js
@@ -1,40 +1,45 @@
 const $ = document.querySelector.bind(document);
 
-function RenderListItem(tabPanes, id) {
-    const container = $('#' + id);
-    const div = document.createElement('div');
-    div.className = 'row';
-    container.appendChild(div);
-
-    const content = tabPanes.filter(item => item.type === id);
-    const html = content.map(item => {
-        const { id, desc, imgSrc_jpg, name } = item;
-        return `
-            <div class="col-3 my-2">
-                <button id="${id}" class="item-btn" title="${desc}">
-                    <img src="${imgSrc_jpg}" alt="" class="img-fluid mx-auto">
-                    <p class="name mt-1">${name}</p>
-                </button>
-            </div>
-        `;
-    });
-
-    div.innerHTML = html.join('');
-}
-
 export function RenderUI(navPills, tabPanes) {
+    function RenderListItem(tabPanes, id) {
+
+        // Create element div have class 'row'.
+        const container = $('#' + id);
+        const div = document.createElement('div');
+        div.className = 'row';
+        container.appendChild(div);
+        
+        // Render cloth item
+        const content = tabPanes.filter(item => item.type === id);
+        const html = content.map(item => {
+            const { id, desc, imgSrc_jpg, name } = item;
+            return `
+                <div class="col-3 my-2">
+                    <button id="${id}" class="item-btn" title="${desc}">
+                        <img src="${imgSrc_jpg}" alt="" class="img-fluid mx-auto">
+                        <p class="name mt-1">${name}</p>
+                    </button>
+                </div>
+            `;
+        });
+    
+        div.innerHTML = html.join('');
+    }
+
     const myTab = $('#myTab');
     const myTabContent = $('#myTabContent');
 
+    // Render navigation
     const nav = navPills.map((item, index) => {
-        const { tabName, type, showName } = item;
+        const { tabName, type } = item;
         return `
             <li class="nav-item" role="presentation">
-                <button class="nav-link ${!index ? 'active' : null}" id="${tabName}" data-toggle="tab" data-target="#${type}" type="button" role="tab" aria-controls="home" aria-selected="true">${showName}</button>
+                <button class="nav-link ${!index ? 'active' : null}" id="${tabName}" data-toggle="tab" data-target="#${type}" type="button" role="tab" aria-controls="home" aria-selected="true"></button>
             </li>
         `;
     });
 
+    // Render tab content
     const tab = navPills.map((item, index) => {
         const { type } = item;
         return `
@@ -45,6 +50,7 @@ export function RenderUI(navPills, tabPanes) {
     myTab.innerHTML = nav.join('');
     myTabContent.innerHTML = tab.join('');
 
+    // Render content
     navPills.forEach(item => {
         const { type } = item;
         RenderListItem(tabPanes, type);
--- controllers/main.js
+++ controllers/main.js
@@ -3,6 +3,20 @@
 import { getData } from '../utils/callData.js';
 import { RenderUI, handleTryOn } from '../controllers/functions.js';
 
+// ================== INIT =========================
+
+const app = async () => {
+    const data = await getData();
+    const { navPills, tabPanes } = data[0];
+
+    RenderUI(navPills, tabPanes);
+    event(tabPanes);
+};
+
+app();
+
+// ==================== EVENT ========================
+
 const event = tabPanes => {
     const myTabContent = document.querySelector('#myTabContent');
 
@@ -14,13 +28,3 @@ const event = tabPanes => {
         }
     };
 };
-
-const app = async () => {
-    const data = await getData();
-    const { navPills, tabPanes } = data[0];
-
-    RenderUI(navPills, tabPanes);
-    event(tabPanes);
-};
-
-app();
